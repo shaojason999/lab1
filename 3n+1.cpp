9d49bb1b84d56ef6e3b5aaa380692e7ffd4db531
shaojason999 <shaojason999@gmail.com>
2017-03-13 23:46:54 +0800

    first program

---
+++ 3n+1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<n<<" ";
+	while(n!=1){
+		if(n%2)
+			n=3*n+1;
+		else
+			n/=2;
+		cout<<n<<" ";
+	}
+	cout<<endl;
+
+
+}
